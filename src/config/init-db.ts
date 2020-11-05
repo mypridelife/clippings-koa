15199c78567a4bf72c4783eb4063ca8e84756d7c
gyh <mypridelife@163.com>
2020-11-05 15:07:57 +0800

    fix: db链接

--- src/config/init-db.ts
+++ src/config/init-db.ts
@@ -1,21 +1,29 @@
 // import * as Sequelize from 'sequelize';
 const Sequelize = require('sequelize');
+var CryptoJS = require('crypto-js');
+var key = require('/www/kindle-key.js');
 
-const sequelize = new Sequelize('xjbq', 'root', 'Mysql000000', {
-    host: '97.64.43.124',
-    dialect: 'mysql',
-    logging: false,
-    define: {
-        underscored: false,
-        freezeTableName: false,
-        charset: 'utf8mb4',
-        dialectOptions: {
-            collate: 'utf8mb4_general_ci'
-        },
-        timestamps: true
+var encryptText =
+  'U2FsdGVkX1+ZlKwtn8vQUd9OeQ3uVx+OKzyXg38hwG7dP2km/4kGTDYvRmJzi9eD';
+var bytes = CryptoJS.AES.decrypt(encryptText, key);
+var originalPassword = bytes.toString(CryptoJS.enc.Utf8);
+
+const sequelize = new Sequelize('xjbq', 'xjbqName', originalPassword, {
+  host: '106.75.169.146',
+  port: '43963',
+  dialect: 'mysql',
+  logging: false,
+  define: {
+    underscored: false,
+    freezeTableName: false,
+    charset: 'utf8mb4',
+    dialectOptions: {
+      collate: 'utf8mb4_general_ci',
     },
-    sync: { force: false },
-    pool: { max: 5, min: 0, idle: 10000 }
+    timestamps: true,
+  },
+  sync: { force: false },
+  pool: { max: 5, min: 0, idle: 10000 },
 });
 
 export default sequelize;
